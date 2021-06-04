4ade763a6a1138646093cdc3a8e8971f3c1a7090
danielmichaels <dans.address@outlook.com>
2021-06-04 18:58:38 +1000

    Add Plausible script

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,6 +13,13 @@ module.exports = {
   projectName: 'documentation', // Usually your repo name.
   themeConfig: {
     colorMode: {disableSwitch: true},
+    scripts: [
+      {src: "https://plausible.io/js/plausible.js",
+        async: true,
+        defer: true,
+        'data-domain': 'docs.mudmap.io'
+      }
+    ],
     // algolia: {
     //   apiKey: '',
     //   indexName: ''
